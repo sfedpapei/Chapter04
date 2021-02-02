1ef9af90cb794c6e186b5a3439c7e97fb3ca7221
sfedpapei <nikoskouklos@gmail.com>
2021-02-02 11:02:14 +0200

    MethodInvokingTaskletAdapter with param

--- src/main/java/com/example/Chapter04/batch/MethodInvokingTaskletConfiguration.java
+++ src/main/java/com/example/Chapter04/batch/MethodInvokingTaskletConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.MethodInvokingTaskletAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -33,16 +35,19 @@ public class MethodInvokingTaskletConfiguration {
 	@Bean
 	public Step methodInvokingStep() {
 		return this.stepBuilderFactory.get("methodInvokingStep")
-					.tasklet(methodInvokingTasklet())
+					.tasklet(methodInvokingTasklet(null))
 					.build();
 	}
 
+	@StepScope
 	@Bean
-	public MethodInvokingTaskletAdapter methodInvokingTasklet() {
+	public MethodInvokingTaskletAdapter methodInvokingTasklet(
+			@Value("#{jobParameters['message']}") String message) {
 		MethodInvokingTaskletAdapter methodInvokingTaskletAdapter = new MethodInvokingTaskletAdapter();
 
 		methodInvokingTaskletAdapter.setTargetObject(service());
 		methodInvokingTaskletAdapter.setTargetMethod("serviceMethod");
+		methodInvokingTaskletAdapter.setArguments(new String[] {message});
 
 		return methodInvokingTaskletAdapter;
 	}
